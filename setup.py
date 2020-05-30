437cfe017701cd1c2bd1057ee8a5fe05c0dac6b9
MarkusZopf <markus.zopf@outlook.com>
2020-05-30 13:37:13 +0200

    Add pypi publication setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  name = 'experiment_tracker',
+  version = '0.0.3',
+  author = 'Markus Zopf',
+  description = 'Simple tool to track experiments by writing parameters and corresponding results into an SQL database.',
+  url = 'https://github.com/MarkusZopf/ExperimentTracker',
+  packages = ['experiment_tracker'],
+  install_requires=[
+          'pymysql',
+      ],
+  classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+  ],
+   python_requires='>=3.6.7',
+)
