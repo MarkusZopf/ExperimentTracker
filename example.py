f1140ac0236b2be4fabddb2f11644309f8941740
MarkusZopf <markus.zopf@outlook.com>
2020-05-30 13:13:38 +0200

    Move database config to example file and add existing_columns cache

--- example.py
+++ example.py
@@ -1,16 +1,23 @@
 from experiment_tracker import Experiment
 
-# 1. Create an experiment. The project name is used to determine the table name of the corresponding SQL table. All experiments of the same project will be stored in the same table.
-experiment = Experiment(project_name='hello_world_project')
+# 1. Specify the database connection details.
+database_config = {'host': 'localhost', 
+                   'port': 3306, 
+                   'user': 'root', 
+                   'passwd': 'root', 
+                   'db': 'experiment_tracker'}
 
-# 2. Configure parameters of the project such as dataset name, or learning rate. Each parameter will be stored in an SQL column in the project's SQL table. The columns is created automatically and type of the column is determined based on the type of the value of the parameter. 
+# 2. Create an experiment. The project name is used to determine the table name of the corresponding SQL table. All experiments of the same project will be stored in the same table.
+experiment = Experiment(database_config=database_config, project_name='hello_world_project')
+
+# 3. Configure parameters of the project such as dataset name, or learning rate. Each parameter will be stored in an SQL column in the project's SQL table. The columns is created automatically and type of the column is determined based on the type of the value of the parameter. 
 experiment.parameters['dataset_train'] = 'train_set'
 experiment.parameters['dataset_test'] = 'test_set'
 experiment.parameters['learning_rate'] = 0.001
 experiment.parameters['num_runs'] = 3
 experiment.parameters['use_dropout'] = False
 
-# 3. Run experiments and collect results.
+# 4. Run experiments and collect results.
 # dataset_train = load_dataset(experiment.parameters['dataset_train'])
 # dataset_test = load_dataset(experiment.parameters['dataset_test'])
 for seed in range(0, experiment.parameters['num_runs']):
@@ -22,7 +29,7 @@ for seed in range(0, experiment.parameters['num_runs']):
 	# evaluation = evaluate_model(dataset_test) # results can be collected at different locations in the code
 	experiment.results['test_loss'] = 0.3
 
-	# 4. Save parameters and corresponding results to the database.
+	# 5. Save parameters and corresponding results to the database.
 	experiment.save_results()
 
 # The stored results can be analyzed later, e.g. by directly querying the database. 
--- experiment_tracker/Experiment.py
+++ experiment_tracker/Experiment.py
@@ -1,13 +1,14 @@
-from experiment_tracker.database_connection import get_database_connection, database_config
+import pymysql
 import time
 
 class Experiment():
 	parameters = dict()
 	results = dict()
 
-	def __init__(self, project_name, add_timestamp=True):
+	def __init__(self, database_config, project_name, add_timestamp=True):
+		self.database_config = database_config
 		self.project_name = project_name
-		self.database_connection = get_database_connection()
+		self.database_connection = self.get_database_connection()
 
 		cursor = self.database_connection.cursor()
 		cursor._defer_warnings = True
@@ -17,6 +18,15 @@ class Experiment():
 
 		if add_timestamp:
 			self.parameters['timestamp'] = str(int(time.time()))
+
+	def get_database_connection(self):
+		database_connection = pymysql.connect(host = self.database_config['host'],
+											  port = self.database_config['port'],
+											  user = self.database_config['user'],
+											  passwd = self.database_config['passwd'],
+											  db = self.database_config['db'],
+											  cursorclass = pymysql.cursors.DictCursor)
+		return database_connection
 
 	def save_results(self, clear_results=True):
 		cursor = self.database_connection.cursor()
@@ -51,10 +61,17 @@ class Experiment():
 		values = values[:-1]
 		return columns, values
 
+	existing_columns = []
 	def does_column_already_exist(self, cursor, column_name):
-		cursor.execute('SHOW COLUMNS FROM ' + self.project_name + ' LIKE %(column_name)s;', {'table_name': self.project_name, 'column_name': column_name})
-		does_column_already_exist = len(cursor.fetchall()) == 1
-		return does_column_already_exist
+		if column_name in self.existing_columns:
+			return True
+
+		else:
+			cursor.execute('SHOW COLUMNS FROM ' + self.project_name + ' LIKE %(column_name)s;', {'table_name': self.project_name, 'column_name': column_name})
+			does_column_already_exist = len(cursor.fetchall()) == 1
+			if does_column_already_exist:
+				self.existing_columns.append(column_name)
+			return does_column_already_exist
 
 	def create_column(self, cursor, column_name, value):
 		if type(value) is bool:
